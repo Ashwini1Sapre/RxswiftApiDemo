e152ba7083f6147f2cfd46d72508cbf9290583fc
ashwinisapreknoxpo <ashwini.sapre@knoxpo.com>
2021-04-20 12:15:39 +0530

    Initial Commit

---
+++ RxswiftApiDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RxswiftApiDemo
+//
+//  Created by Knoxpo MacBook Pro on 20/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
